5e463d99b570fd5ebf9f1777af00c14efda6c7e3
Loren Yu <yuloren@gmail.com>
2013-01-27 23:34:48 -0800

    Added a advantage strategy

--- RiskProbabilities.py
+++ RiskProbabilities.py
@@ -9,11 +9,13 @@ def attack(args):
 
 def win_territory(args):
     if args.strategy == 'all-in':
-        strategy = strategies.allIn
+        strategy = strategies.all_in
     elif args.strategy == 'advantage':
         strategy = strategies.advantage
+    elif args.strategy == 'outnumber':
+        strategy = strategies.outnumber
     else:
-        strategy = strategies.allIn
+        strategy = strategies.all_in
     print p.p2win((args.num_attackers, args.num_defenders, strategy))
 
 parser = argparse.ArgumentParser(description='Compute probabilities within the game of Risk.')
@@ -27,7 +29,7 @@ attack_parser.set_defaults(func=attack)
 win_territory_parser = subparsers.add_parser('win-territory', help='Compute probability of winning a territory given a particular strategy')
 win_territory_parser.add_argument('num_attackers', type=int, help='Number of units in attacking territory')
 win_territory_parser.add_argument('num_defenders', type=int, help='Number of units in defending territory')
-win_territory_parser.add_argument('-s', '--strategy', choices=['all-in', 'advantage'], default='all-in', help="""Attacking strategy.
+win_territory_parser.add_argument('-s', '--strategy', choices=['all-in', 'advantage', 'outnumber'], default='all-in', help="""Attacking strategy.
 
     all-in:     Attack with as many units as possible until victory or unable to attack.
     advantage:  Attack with as many units as possible as long as the number of units in the attacking territory, excluding the 1 unit you have to leave in the territory, outnumbers the number of units in the defending territory.""")
--- strategies.py
+++ strategies.py
@@ -1,5 +1,9 @@
-def allIn(numUnitsInAttackingCountry, numUnitsInDefendingCountry):
+def all_in(numUnitsInAttackingCountry, numUnitsInDefendingCountry):
     return min(numUnitsInAttackingCountry - 1, 3) if numUnitsInDefendingCountry > 0 else 0
 
 def advantage(numUnitsInAttackingCountry, numUnitsInDefendingCountry):
+    return min(numUnitsInAttackingCountry - 1, 3) if (numUnitsInAttackingCountry - 1 > min(numUnitsInDefendingCountry, 2)) and (numUnitsInDefendingCountry > 0) else 0
+
+def outnumber(numUnitsInAttackingCountry, numUnitsInDefendingCountry):
     return min(numUnitsInAttackingCountry - 1, 3) if (numUnitsInAttackingCountry - 1 > numUnitsInDefendingCountry) and (numUnitsInDefendingCountry > 0) else 0
+
